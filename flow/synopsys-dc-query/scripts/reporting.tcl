6fb9f99c78b0c5c8519dcb8222c54d07f3ea1398
Alex Carsello <alex.carsello@gmail.com>
2021-03-19 16:37:05 -0700

    Generate query script and run timing query

--- flow/synopsys-dc-query/scripts/reporting.tcl
+++ flow/synopsys-dc-query/scripts/reporting.tcl
@@ -1,16 +1,11 @@
 #=========================================================================
 # reporting.tcl
 #=========================================================================
-# Final reports
+# Generate Report from query
 #
-# Author : Christopher Torng
-# Date   : May 14, 2018
+# Author : Alex Carsello
+# Date   : March 19, 2021
 #
 
-report_timing \
-  -input_pins -capacitance -transition_time \
-  -nets -significant_digits 4 -nosplit      \
-  -path_type full_clock -attributes         \
-  -nworst 10 -max_paths 30 -delay_type max  \
-  > reports/timing_query.rpt
+source ../query.tcl
 
