78ef3a7e3f4baaeb42aa6a77c8f8b13d2b2a294e
Raj Setaluri <raj.setaluri@gmail.com>
2021-04-19 15:42:33 -0500

    Use test fixture for generate_paths_test.py

--- pdq/circuit_tools/generate_paths_test.py
+++ pdq/circuit_tools/generate_paths_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from designs.simple_alu import SimpleAlu
 from pdq.circuit_tools.circuit_utils import find_instances_by_name
 from pdq.circuit_tools.generate_paths import SignalPathQuery, generate_paths
@@ -5,26 +7,47 @@ from pdq.circuit_tools.signal_path import SignalPath, paths_equal, validate_path
 from pdq.common.algorithms import only
 
 
-def test_basic():
-    query = SignalPathQuery(SimpleAlu.a[0], SimpleAlu.out[0])
-    paths = generate_paths(SimpleAlu, query)
+class _GeneratePathsTest:
+    def __init__(self):
+        self.ckt = SimpleAlu
+        self._instance_cache = {}
 
-    # Check that all the paths returned are valid paths w.r.t. SimpleAlu.
-    assert all(validate_path(path, SimpleAlu) for path in paths)
+    def generate_paths(self, query):
+        self.paths = generate_paths(self.ckt, query)
+        # Check that all the paths returned are valid paths w.r.t. ckt.
+        assert all(validate_path(path, self.ckt) for path in self.paths)
+
+    def get_instance(self, name):
+        try:
+            return self._instance_cache[name]
+        except KeyError:
+            pass
+        inst = only(find_instances_by_name(self.ckt, name))
+        self._instance_cache[name] = inst
+        return inst
+
+    def pop_path(self, path):
+        # Since the set of paths returned by generate_paths can be in any order,
+        # we do the following: for each path we expect to be returned, find it's
+        # index in the returned list, and remove it. Then, at the end, we expect
+        # the list to be empty.
+        idx = only((
+            i for i, p in enumerate(self.paths) if paths_equal(p, path)))
+        self.paths.pop(idx)
+
+
+@pytest.fixture
+def generate_paths_test():
+    return _GeneratePathsTest()
+
+
+def test_basic(generate_paths_test):
+    query = SignalPathQuery(SimpleAlu.a[0], SimpleAlu.out[0])
+    generate_paths_test.generate_paths(query)
 
     # Grab the internal instances of SimpleAlu for checking the paths.
-    units = (only(find_instances_by_name(SimpleAlu, name))
-             for name in ("add", "sub"))
-    mux = only(find_instances_by_name(SimpleAlu, "Mux"))
-
-    # Since the set of paths returned by generate_paths can be in any order, we
-    # do the following: for each path we expect to be returned, find it's index
-    # in the returned list, and remove it. Then, at the end, we expect the list
-    # to be empty.
-
-    def _find_and_remove(path: SignalPath):
-        idx = only((i for i, p in enumerate(paths) if paths_equal(p, path)))
-        paths.pop(idx)
+    units = (generate_paths_test.get_instance(name) for name in ("add", "sub"))
+    mux = generate_paths_test.get_instance("Mux")
 
     # Check all the paths through the functional units (add, sub).
     for i, unit in enumerate(units):
@@ -35,14 +58,14 @@ def test_basic():
                 dst=SimpleAlu.out[0],
                 path=[(unit.I0[0], bit),
                       (mux_in[j], mux.O[0])])
-            _find_and_remove(path)
+            generate_paths_test.pop_path(path)
 
     # Check the passthrough path.
-    _find_and_remove(
+    generate_paths_test.pop_path(
         SignalPath(
             src=SimpleAlu.a[0],
             dst=SimpleAlu.out[0],
             path=[(mux.I2[0], mux.O[0])]))
 
     # Check that we've found all the paths.
-    assert len(paths) == 0
+    assert len(generate_paths_test.paths) == 0
