5310242aeec99923816fcc87969413d7165092a2
Raj Setaluri <raj.setaluri@gmail.com>
2021-04-20 14:14:46 -0500

    Factor common parts of basic_flow_main.py

--- basic_flow_main.py
+++ basic_flow_main.py
@@ -1,10 +1,10 @@
 import argparse
-import importlib
-import inspect
 import logging
 import pathlib
 
 from pdq.common.reporting import make_header
+from pdq.common.main_utils import (
+    add_design_arguments, parse_design_args, slice_args)
 from pdq.flow_tools.basic_flow import make_basic_flow, BasicFlowOpts
 from pdq.report_parsing.parsers import parse_dc_area
 from pdq.report_parsing.parsers import parse_dc_timing
@@ -59,48 +59,13 @@ def _main(ckt, opts):
     print (make_header("", pad=False))
 
 
-def _make_params(gen, args):
-    if args.params is None:
-        return {}
-    gen_sig = inspect.signature(gen.__init__)
-    parser = argparse.ArgumentParser(add_help=False, prog=gen.__name__)
-    for gen_sig_param in gen_sig.parameters.values():
-        if gen_sig_param.name == "self":
-            continue
-        kwargs = {}
-        if gen_sig_param.annotation is not inspect.Parameter.empty:
-            kwargs["type"] = gen_sig_param.annotation
-        if gen_sig_param.default is not inspect.Parameter.empty:
-            kwargs["default"] = gen_sig_param.default
-        parser.add_argument(f"-{gen_sig_param.name}", **kwargs)
-    params = ["-" + p for p in args.params.split(",")]
-    return vars(parser.parse_args(params))
-
-
-def _get_ckt(args):
-    py_module = importlib.import_module(args.package)
-    if args.module is not None:
-        return getattr(py_module, args.module)
-    if args.generator is not None:
-        gen = getattr(py_module, args.generator)
-        params = _make_params(gen, args)
-        logging.info(f"Generator params {params}")
-        return gen(**params)
-    raise NotImplementedError()
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("package", type=str)
-    ckt_group = parser.add_mutually_exclusive_group(required=True)
-    ckt_group.add_argument("--module", type=str)
-    ckt_group.add_argument("--generator", type=str)
-    parser.add_argument("--params", type=str)
-    opts_group = parser.add_argument_group("opts")
-    opts_group.add_argument("--clock_period", type=float, default=2.0)
+    design_grp = add_design_arguments(parser)
+    opts_grp = parser.add_argument_group("opts")
+    opts_grp.add_argument("--clock_period", type=float, default=2.0)
     args = parser.parse_args()
-    ckt = _get_ckt(args)
-    opts_keys = list(action.dest for action in opts_group._group_actions)
-    opts = {k: getattr(args, k) for k in opts_keys}
+    ckt = parse_design_args(slice_args(args, design_grp))
+    opts = vars(slice_args(args, opts_grp))
     logging.info(f"Running with opts {opts}")
     _main(ckt, opts)
---
+++ pdq/common/main_utils.py
@@ -0,0 +1,83 @@
+import argparse
+import functools
+import importlib
+import inspect
+import logging
+
+
+class _ParseArgError(BaseException):
+    pass
+
+
+class _NoArgError(_ParseArgError):
+    pass
+
+
+class _ArgValueError(_ParseArgError):
+    pass
+
+
+class _WrappedNamespace(argparse.Namespace):
+    def __getattribute__(self, key):
+        try:
+            return super().__getattribute__(key)
+        except AttributeError:
+            raise _NoArgError() from None
+
+
+def _arg_parser(fn):
+
+    @functools.wraps(fn)
+    def _wrapper(args: argparse.Namespace):
+        args = _WrappedNamespace(**vars(args))
+        return fn(args)
+
+    return _wrapper
+
+
+def _parse_gen_params(gen, args: argparse.Namespace):
+    if args.params is None:
+        return {}
+    gen_sig = inspect.signature(gen.__init__)
+    parser = argparse.ArgumentParser(add_help=False, prog=gen.__name__)
+    for gen_sig_param in gen_sig.parameters.values():
+        if gen_sig_param.name == "self":
+            continue
+        kwargs = {}
+        if gen_sig_param.annotation is not inspect.Parameter.empty:
+            kwargs["type"] = gen_sig_param.annotation
+        if gen_sig_param.default is not inspect.Parameter.empty:
+            kwargs["default"] = gen_sig_param.default
+        parser.add_argument(f"-{gen_sig_param.name}", **kwargs)
+    params = ["-" + p for p in args.params.split(",")]
+    return vars(parser.parse_args(params))
+
+
+def slice_args(args: argparse.Namespace, grp):
+    keys = (action.dest for action in grp._group_actions)
+    return argparse.Namespace(**{k: getattr(args, k) for k in keys})
+
+
+def add_design_arguments(parser: argparse.ArgumentParser):
+    design_grp = parser.add_argument_group("design")
+    design_grp.add_argument("--package", type=str, required=True)
+    name_grp = design_grp.add_mutually_exclusive_group(required=True)
+    name_grp.add_argument("--module", type=str)
+    name_grp.add_argument("--generator", type=str)
+    design_grp.add_argument("--params", type=str)
+    return design_grp
+
+
+@_arg_parser
+def parse_design_args(args: argparse.Namespace):
+    if args.package is None:
+        raise _ArgValueError()
+    py_module = importlib.import_module(args.package)
+    if args.module is not None:
+        return getattr(py_module, args.module)
+    if args.generator is not None:
+        gen = getattr(py_module, args.generator)
+        params = _parse_gen_params(gen, args)
+        logging.info(f"Generator params {params}")
+        return gen(**params)
+    raise _ArgValueError()
