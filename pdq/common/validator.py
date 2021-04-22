4ebed7aeed050f42b7f1673bf2e002715b166fc6
Raj Setaluri <raj.setaluri@gmail.com>
2021-04-22 18:38:05 -0500

    Add support for extraction with hierarchical paths

--- pdq/common/validator.py
+++ pdq/common/validator.py
@@ -1,6 +1,22 @@
 import dataclasses
 import functools
 import logging
+from typing import Optional
+
+
+_live_validator = False
+
+
+class _ValidatorContext:
+    def __enter__(*args, **kwargs):
+        global _live_validator
+        assert not _live_validator
+        _live_validator = True
+
+    def __exit__(*args, **kwargs):
+        global _live_validator
+        assert _live_validator
+        _live_validator = False
 
 
 @dataclasses.dataclass(frozen=True)
@@ -29,11 +45,16 @@ class ValidatorResult:
 def validator(fn):
 
     @functools.wraps(fn)
-    def _wrapper(*args, **kwargs) -> ValidatorResult:
-        try:
+    def _wrapper(*args, **kwargs) -> Optional[ValidatorResult]:
+        global _live_validator
+        if _live_validator:
             fn(*args, **kwargs)
-        except AssertionError as e:
-            return ValidatorResult.make_error(e)
-        return ValidatorResult.make_ok()
+            return
+        with _ValidatorContext():
+            try:
+                fn(*args, **kwargs)
+            except AssertionError as e:
+                return ValidatorResult.make_error(e)
+            return ValidatorResult.make_ok()
 
     return _wrapper
