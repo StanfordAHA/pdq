e8bb4494a5438b2e967a3dce5c4a88aa480a6d4c
Raj Setaluri <raj.setaluri@gmail.com>
2021-10-18 16:37:07 -0700

    Add comb designs

---
+++ designs/comb_designs.py
@@ -0,0 +1,12 @@
+import magma as m
+
+
+class Comb0(m.Circuit):
+    T = m.Bits[16]
+    io = m.IO(a=m.In(T), b=m.In(T), c=m.In(T), y=m.Out(T), z=m.Out(T))
+    a = ~io.a
+    o = io.a | a
+    o = o & io.b
+    io.y @= o
+    io.z @= o | (io.c ^ io.a)
+
